c240ad7d3e83dc2cfdf787867010bf28bbb0123b
Karan Hemraj Patil <karanhemrajpatil1907@gmail.com>
2024-06-16 22:37:51 +0530

    changes commited

--- src/Data.jsx
+++ src/Data.jsx
@@ -23,11 +23,11 @@ import image15 from "./assets/images/images15.jpeg"
 
 
 
-export const  behance_logo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Behance_logo.svg/1280px-Behance_logo.svg.png';
+export const  behance_logo = image1;
 
-export const  adobe_cloude = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Adobe_Creative_Cloud_rainbow_icon.svg/640px-Adobe_Creative_Cloud_rainbow_icon.svg.png';
+export const  adobe_cloude = image1;
 
-export const adobeLogo= 'https://cdn.iconscout.com/icon/free/png-256/adobe-254-761738.png';
+export const adobeLogo= image1;
 
 
 export const searchType = [
